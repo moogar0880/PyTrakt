236e192405068ea761db11aae168d8241180f439
moogar0880 <moogar@comcast.net>
2014-05-31 19:24:37 -0400

    Better docstrings

--- trakt/errors.py
+++ trakt/errors.py
@@ -4,12 +4,17 @@ __all__ = ['TraktException', 'InvalidAPIKey', 'InvalidCredentials']
 
 
 class TraktException(BaseException):
+    """Base Exception type for trakt module"""
     pass
 
 
 class InvalidAPIKey(TraktException):
+    """Custom trakt exception to be raised if the provided API Key is invalid"""
     pass
 
 
 class InvalidCredentials(TraktException):
+    """Custom trakt exception to be raised if a provided username and password
+    are determined to be invalid
+    """
     pass

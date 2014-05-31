6e4ffdf535036160dc1e43d861519483fd1a540a
moogar0880 <moogar@comcast.net>
2014-05-31 19:03:49 -0400

    Added sphinx_bootstrap theme

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,7 @@
 import sys
 import os
 import trakt
+import sphinx_bootstrap_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -101,7 +102,9 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+# html_theme = 'default'
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the

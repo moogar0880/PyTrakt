329cc8bee0220e98e426b0b5f8585f1a22ac9f1f
moogar0880 <moogar@comcast.net>
2014-06-01 08:42:09 -0400

    Removing bootstrap theme for now.

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@
 import sys
 import os
 import trakt
-import sphinx_bootstrap_theme
+# import sphinx_bootstrap_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -102,23 +102,23 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-# html_theme = 'default'
-html_theme = 'bootstrap'
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+html_theme = 'default'
+# html_theme = 'bootstrap'
+# html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
+# html_theme_options = {
     # Location of link to source.
     # Options are "nav" (default), "footer" or anything else to exclude.
-    'source_link_position': 'null',
+    # 'source_link_position': 'null',
     # Bootswatch (http://bootswatch.com/) theme.
     #
     # Options are nothing with "" (default) or the name of a valid theme
     # such as "amelia" or "cosmo".
-    'bootswatch_theme': 'flatly',
-}
+    # 'bootswatch_theme': 'flatly',
+# }
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []

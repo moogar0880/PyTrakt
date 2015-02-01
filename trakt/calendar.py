3a771c330bad816f6f380c53cb94ad3f4ff77a26
moogar0880 <moogar0880@gmail.com>
2015-01-31 21:43:48 -0500

    Updating calendar functionality to work with the trakt v2 API

--- trakt/calendar.py
+++ trakt/calendar.py
@@ -1,16 +1,12 @@
 """Interfaces to all of the Calendar objects offered by the Trakt.tv API"""
-import sys
 from datetime import datetime
-if int(sys.version[0]) == 2:
-    from urllib import urlencode
-elif int(sys.version[0]) == 3:
-    from urllib.parse import urlencode
 
 from . import BaseAPI
 from .tv import TVEpisode
-import trakt
+
 __author__ = 'Jon Nappi'
-__all__ = ['Calendar', 'PremiereCalendar', 'ShowCalendar', 'UserCalendar']
+__all__ = ['Calendar', 'PremiereCalendar', 'ShowCalendar', 'SeasonCalendar',
+           'MovieCalendar']
 
 
 def _now_date_format():
@@ -19,15 +15,23 @@ def _now_date_format():
     year = now.year
     month = now.month if now.month > 10 else '0{}'.format(now.month)
     day = now.day if now.day > 10 else '0{}'.format(now.day)
-    date = int('{}{}{}'.format(year, month, day))
-    return date
+    return '{}-{}-{}'.format(year, month, day)
+
+
+def airs_date(airs_at):
+    """convert a timestamp of the form '2015-02-01T05:30:00.000-08:00' to a
+    python datetime object (with time zone information removed)
+    """
+    convertable = '-'.join(airs_at.split('-')[:-1])
+    return datetime.strptime(convertable, '%Y-%m-%dT%H:%M:%S.000')
 
 
 class Calendar(BaseAPI):
     """Base :class:`Calendar` type serves as a foundation for other Calendar
     types
     """
-    def __init__(self, date=None, days=None):
+
+    def __init__(self, date=None, days=7):
         """Create a new :class:`Calendar` object
 
         :param date: Start date of this :class:`Calendar` in the format Ymd
@@ -37,7 +41,7 @@ class Calendar(BaseAPI):
         """
         super(Calendar, self).__init__()
         self.date = date or _now_date_format()
-        self.days = days or 7
+        self.days = days
         self.url = None
         self._episodes = []
 
@@ -57,50 +61,59 @@ class Calendar(BaseAPI):
 
     def _build_uri(self):
         """construct the fully formatted url for this Calendar"""
-        url = self.url
-        url_args = {'date': self.date, 'days': self.days}
-        formatted_url_args = urlencode({x: url_args[x] for x in url_args if
-                                        url_args[x] is not None})
-        if formatted_url_args != {}:
-            url = '/'.join([url, '?', formatted_url_args])
-        return url
+        return '/'.join([self.url, str(self.date), str(self.days)])
 
     def _build(self):
         """Build the calendar"""
         if self.url is not None:
             ext = self._build_uri()
-            data_list = self._get_(ext)
+            data_dict = self._get_(ext)
             self._episodes = []
-            for data in data_list:
-                for episode in data['episodes']:
-                    show = episode['show']['title']
-                    season = episode['episode']['season']
-                    ep = episode['episode']['number']
-                    ep_data = episode['episode']
+            for date in data_dict:
+                episodes = data_dict.get(date, [])
+                for episode in episodes:
+                    show = episode.get('show', {}).get('title')
+                    season = episode.get('episode', {}).get('season')
+                    ep = episode.get('episode', {}).get('number')
+                    data = {'airs_at': airs_date(episode.get('airs_at')),
+                            'episode_ids': episode.get('episode').get('ids'),
+                            'title': episode.get('episode', {}).get('title')}
                     self._episodes.append(TVEpisode(show, ep, season,
-                                                    episode_data=ep_data))
+                                                    episode_data=data))
+            self._episodes = sorted(self._episodes, key=lambda x: x.airs_at)
 
 
 class PremiereCalendar(Calendar):
     """All shows premiering during the time period specified."""
-    def __init__(self, *args, **kwargs):
-        super(PremiereCalendar, self).__init__(*args, **kwargs)
-        self.url = 'calendar/premieres.json/{}'.format(trakt.api_key)
+
+    def __init__(self, date=None, days=7):
+        super(PremiereCalendar, self).__init__(date=date, days=days)
+        self.url = 'calendars/shows/new'
         self._build()
 
 
 class ShowCalendar(Calendar):
     """TraktTV ShowCalendar"""
-    def __init__(self, *args, **kwargs):
-        super(ShowCalendar, self).__init__(*args, **kwargs)
-        self.url = 'calendar/shows.json/{}'.format(trakt.api_key)
+
+    def __init__(self, date=None, days=7):
+        super(ShowCalendar, self).__init__(date=date, days=days)
+        self.url = 'calendars/shows'
         self._build()
 
 
-class UserCalendar(Calendar):
-    def __init__(self, user_name, *args, **kwargs):
-        super(UserCalendar, self).__init__(*args, **kwargs)
-        self.user_name = user_name
-        self.url = 'user/calendar/shows.json/{}/{}'.format(trakt.api_key,
-                                                           self.user_name)
+class SeasonCalendar(Calendar):
+    """TraktTV TV Show Season Premiere"""
+
+    def __init__(self, date=None, days=7):
+        super(SeasonCalendar, self).__init__(date=date, days=days)
+        self.url = 'calendars/shows/premieres'
+        self._build()
+
+
+class MovieCalendar(Calendar):
+    """TraktTV Movie Calendar"""
+
+    def __init__(self, date=None, days=7):
+        super(MovieCalendar, self).__init__(date=date, days=days)
+        self.url = 'calendars/movies'
         self._build()

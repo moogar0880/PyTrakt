31e8e8611dd5e0e07804f79d83f29aec7c76ccb4
moogar0880 <moogar0880@gmail.com>
2015-03-04 22:34:48 -0500

    Reconfigured auth system and request handling

--- trakt/__init__.py
+++ trakt/__init__.py
@@ -1,23 +1,9 @@
 """A wrapper for the Trakt.tv REST API"""
 try:
-    from ._core import *
+    from .core import *
 except ImportError:
     pass
 
-version_info = (1, 0, 3)
+version_info = (2, 0, 0)
 __author__ = 'Jon Nappi'
 __version__ = '.'.join([str(i) for i in version_info])
-
-
-@property
-def api_key():
-    """The current api key for accessing the Trakt.tv REST API system"""
-    if '_TRAKT_APIKEY_' not in globals():
-        globals()['_TRAKT_APIKEY_'] = None
-    return globals()['_TRAKT_APIKEY_']
-@api_key.setter
-def api_key(value):
-    globals()['_TRAKT_APIKEY_'] = value
-@api_key.deleter
-def api_key():
-    globals()['_TRAKT_APIKEY_'] = None

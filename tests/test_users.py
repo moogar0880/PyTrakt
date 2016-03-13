fc0e22facc124055af8a7e4dde3897d2d6f4a3dc
moogar0880 <moogar0880@gmail.com>
2016-03-12 20:34:28 -0500

    added additional user tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -10,25 +10,29 @@ def test_user_settings():
 
 
 def test_requests():
-    requests = get_all_requests()
-    assert isinstance(requests, list)
-    assert all([isinstance(r, Request) for r in requests])
-    for request in requests:
-        r = request.approve()
-        assert r is None
-        r = request.deny()
-        assert r is None
+    getters = [get_all_requests, User('sean').get_follower_requests]
+    for getter in getters:
+        requests = getter()
+        assert isinstance(requests, list)
+        assert all([isinstance(r, Request) for r in requests])
+        for request in requests:
+            r = request.approve()
+            assert r is None
+            r = request.deny()
+            assert r is None
 
 
-def test_get_user():
+def test_user():
     sean = User('sean')
     assert sean.username == 'sean'
+    assert str(sean) == '<User>: sean'
 
 
 def test_user_collections():
     sean = User('sean')
-    assert all([isinstance(m, Movie) for m in sean.movie_collection])
-    assert all([isinstance(s, TVShow) for s in sean.show_collection])
+    for _ in range(2):
+        assert all([isinstance(m, Movie) for m in sean.movie_collection])
+        assert all([isinstance(s, TVShow) for s in sean.show_collection])
 
 
 def test_user_list():
@@ -46,6 +50,7 @@ def test_user_list():
 
     # get list
     l = UserList.get(data['name'], sean.username)
+    l = sean.get_list(data['name'])
     for k, v in data.items():
         assert getattr(l, k) == v
 
@@ -61,6 +66,9 @@ def test_user_list():
     l.like()
     l.unlike()
 
+    # just test to ensure that iterating over list items works
+    l.__iter__()
+
     # delete entire list
     l.delete_list()
 
@@ -73,9 +81,10 @@ def test_follow_user():
 
 def test_get_others():
     sean = User('sean')
-    assert all([isinstance(u, User) for u in sean.followers])
-    assert all([isinstance(u, User) for u in sean.following])
-    assert all([isinstance(u, User) for u in sean.friends])
+    for _ in range(2):
+        assert all([isinstance(u, User) for u in sean.followers])
+        assert all([isinstance(u, User) for u in sean.following])
+        assert all([isinstance(u, User) for u in sean.friends])
 
 
 def test_user_ratings():
@@ -83,12 +92,14 @@ def test_user_ratings():
     rating_types = ['movies', 'shows', 'seasons', 'episodes']
     for typ in rating_types:
         assert all([isinstance(r, dict) for r in sean.get_ratings(typ)])
+    assert all([isinstance(r, dict) for r in sean.get_ratings('movies', 10)])
 
 
 def test_user_watchlists():
     sean = User('sean')
-    assert all([isinstance(m, Movie) for m in sean.watchlist_movies])
-    assert all([isinstance(s, TVShow) for s in sean.watchlist_shows])
+    for _ in range(2):
+        assert all([isinstance(m, Movie) for m in sean.watchlist_movies])
+        assert all([isinstance(s, TVShow) for s in sean.watchlist_shows])
 
 
 def test_watching():
@@ -103,5 +114,11 @@ def test_watching():
 
 def test_watched():
     sean = User('sean')
-    assert all([isinstance(m, Movie) for m in sean.watched_movies])
-    assert all([isinstance(s, TVShow) for s in sean.watched_shows])
+    for _ in range(2):
+        assert all([isinstance(m, Movie) for m in sean.watched_movies])
+        assert all([isinstance(s, TVShow) for s in sean.watched_shows])
+
+
+def test_stats():
+    sean = User('sean')
+    assert isinstance(sean.get_stats(), dict)

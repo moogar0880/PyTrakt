6683794cd595acfd55384bdf0b700e77c71c87af
Elan Ruusamäe <glen@pld-linux.org>
2022-01-13 11:28:04 +0200

    Test tvshow

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,8 +2,12 @@
 """trakt.tv functional tests"""
 from trakt.api import TraktApi
 from trakt.core import get_api
+from trakt.tv import TVShow
 
 
 def test_api():
     api = get_api()
     assert isinstance(api, TraktApi)
+
+    show = TVShow('Game of Thrones')
+    assert show.title == 'Game of Thrones'

c95eeeb01f8414687edf34bdbe1cc89d07b9d29a
Elan Ruusamäe <glen@pld-linux.org>
2022-01-14 00:11:18 +0200

    Style fix

--- trakt/auth/device.py
+++ trakt/auth/device.py
@@ -84,8 +84,7 @@ class DeviceAuth:
         data = {"client_id": self.config.CLIENT_ID}
         device_response = self.client.post('/oauth/device/code', data=data)
 
-        print('Your user code is: {user_code}, please navigate to '
-              '{verification_url} to authenticate'.format(
+        print('Your user code is: {user_code}, please navigate to {verification_url} to authenticate'.format(
             user_code=device_response.get('user_code'),
             verification_url=device_response.get('verification_url')
         ))

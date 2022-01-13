c4f45160f4ee7abd896de9f44f3ac766b60a8dc9
Elan Ruusamäe <glen@pld-linux.org>
2022-01-13 17:35:43 +0200

    Update test to test for something that is mocked as well

--- tests/test_api.py
+++ tests/test_api.py
@@ -13,8 +13,4 @@ def test_api():
 
     show = TVShow('Game of Thrones')
     assert show.title == 'Game of Thrones'
-
-    assert len(show.aliases) == 305
-    alias = show.aliases[0]
-    assert isinstance(alias, Alias)
-    assert alias.title == '冰與火之歌：權力遊戲'
+    assert show.certification == 'TV-MA'

502d3f4781d6631e13e41f1afef60406dff798bf
Elan Ruusamäe <glen@pld-linux.org>
2022-01-13 18:19:33 +0200

    fixup! Add trakt/auth/device_auth.py module

--- trakt/auth/device_auth.py
+++ trakt/auth/device_auth.py
@@ -1,58 +1,141 @@
-def device_auth(client_id=None, client_secret=None, store=False):
-    """Process for authenticating using device authentication.
+import time
 
-    The function will attempt getting the device_id, and provide
-    the user with a url and code. After getting the device
-    id, a timer is started to poll periodic for a successful authentication.
-    This is a blocking action, meaning you
-    will not be able to run any other code, while waiting for an access token.
 
-    If you want more control over the authentication flow, use the functions
-    get_device_code and get_device_token.
-    Where poll_for_device_token will check if the "offline"
-    authentication was successful.
+class DeviceAuth:
+    def device_auth(client_id=None, client_secret=None, store=False):
+        """Process for authenticating using device authentication.
 
-    :param client_id: Your Trakt OAuth Application's Client ID
-    :param client_secret: Your Trakt OAuth Application's Client Secret
-    :param store: Boolean flag used to determine if your trakt api auth data
-        should be stored locally on the system. Default is :const:`False` for
-        the security conscious
-    :return: A dict with the authentication result.
-    Or False of authentication failed.
-    """
-    error_messages = {
-        404: 'Invalid device_code',
-        409: 'You already approved this code',
-        410: 'The tokens have expired, restart the process',
-        418: 'You explicitly denied this code',
-    }
+        The function will attempt getting the device_id, and provide
+        the user with a url and code. After getting the device
+        id, a timer is started to poll periodic for a successful authentication.
+        This is a blocking action, meaning you
+        will not be able to run any other code, while waiting for an access token.
 
-    success_message = (
-        "You've been successfully authenticated. "
-        "With access_token {access_token} and refresh_token {refresh_token}"
-    )
+        If you want more control over the authentication flow, use the functions
+        get_device_code and get_device_token.
+        Where poll_for_device_token will check if the "offline"
+        authentication was successful.
 
-    response = get_device_code(client_id=client_id,
-                               client_secret=client_secret)
-    device_code = response['device_code']
-    interval = response['interval']
+        :param client_id: Your Trakt OAuth Application's Client ID
+        :param client_secret: Your Trakt OAuth Application's Client Secret
+        :param store: Boolean flag used to determine if your trakt api auth data
+            should be stored locally on the system. Default is :const:`False` for
+            the security conscious
+        :return: A dict with the authentication result.
+        Or False of authentication failed.
+        """
+        error_messages = {
+            404: 'Invalid device_code',
+            409: 'You already approved this code',
+            410: 'The tokens have expired, restart the process',
+            418: 'You explicitly denied this code',
+        }
 
-    # No need to check for expiration, the API will notify us.
-    while True:
-        response = get_device_token(device_code, client_id, client_secret,
-                                    store)
+        success_message = (
+            "You've been successfully authenticated. "
+            "With access_token {access_token} and refresh_token {refresh_token}"
+        )
 
+        response = get_device_code(client_id=client_id,
+                                   client_secret=client_secret)
+        device_code = response['device_code']
+        interval = response['interval']
+
+        # No need to check for expiration, the API will notify us.
+        while True:
+            response = get_device_token(device_code, client_id, client_secret,
+                                        store)
+
+            if response.status_code == 200:
+                print(success_message.format_map(response.json()))
+                break
+
+            elif response.status_code == 429:  # slow down
+                interval *= 2
+
+            elif response.status_code != 400:  # not pending
+                print(error_messages.get(response.status_code, response.reason))
+                break
+
+            time.sleep(interval)
+
+        return response
+
+    def get_device_code(client_id=None, client_secret=None):
+        """Generate a device code, used for device oauth authentication.
+
+        Trakt docs: https://trakt.docs.apiary.io/#reference/
+        authentication-devices/device-code
+        :param client_id: Your Trakt OAuth Application's Client ID
+        :param client_secret: Your Trakt OAuth Application's Client Secret
+        :return: Your OAuth device code.
+        """
+        global CLIENT_ID, CLIENT_SECRET, OAUTH_TOKEN
+        if client_id is None and client_secret is None:
+            client_id, client_secret = _get_client_info()
+        CLIENT_ID, CLIENT_SECRET = client_id, client_secret
+        HEADERS['trakt-api-key'] = CLIENT_ID
+
+        device_code_url = urljoin(BASE_URL, '/oauth/device/code')
+        headers = {'Content-Type': 'application/json'}
+        data = {"client_id": CLIENT_ID}
+
+        device_response = session.post(device_code_url,
+                                       json=data, headers=headers).json()
+        print('Your user code is: {user_code}, please navigate to '
+              '{verification_url} to authenticate'.format(
+            user_code=device_response.get('user_code'),
+            verification_url=device_response.get('verification_url')
+        ))
+
+        device_response['requested'] = time.time()
+        return device_response
+
+    def get_device_token(device_code, client_id=None, client_secret=None,
+                         store=False):
+        """
+        Trakt docs: https://trakt.docs.apiary.io/#reference/
+        authentication-devices/get-token
+        Response:
+        {
+          "access_token": "",
+          "token_type": "bearer",
+          "expires_in": 7776000,
+          "refresh_token": "",
+          "scope": "public",
+          "created_at": 1519329051
+        }
+        :return: Information regarding the authentication polling.
+        :return type: dict
+        """
+        global CLIENT_ID, CLIENT_SECRET, OAUTH_TOKEN, OAUTH_REFRESH
+        if client_id is None and client_secret is None:
+            client_id, client_secret = _get_client_info()
+        CLIENT_ID, CLIENT_SECRET = client_id, client_secret
+        HEADERS['trakt-api-key'] = CLIENT_ID
+
+        data = {
+            "code": device_code,
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET
+        }
+
+        response = session.post(
+            urljoin(BASE_URL, '/oauth/device/token'), json=data
+        )
+
+        # We only get json on success.
         if response.status_code == 200:
-            print(success_message.format_map(response.json()))
-            break
+            data = response.json()
+            OAUTH_TOKEN = data.get('access_token')
+            OAUTH_REFRESH = data.get('refresh_token')
+            OAUTH_EXPIRES_AT = data.get("created_at") + data.get("expires_in")
 
-        elif response.status_code == 429:  # slow down
-            interval *= 2
+            if store:
+                _store(
+                    CLIENT_ID=CLIENT_ID, CLIENT_SECRET=CLIENT_SECRET,
+                    OAUTH_TOKEN=OAUTH_TOKEN, OAUTH_REFRESH=OAUTH_REFRESH,
+                    OAUTH_EXPIRES_AT=OAUTH_EXPIRES_AT
+                )
 
-        elif response.status_code != 400:  # not pending
-            print(error_messages.get(response.status_code, response.reason))
-            break
-
-        time.sleep(interval)
-
-    return response
+        return response

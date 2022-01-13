fb82bb861c887128f6d63183173b0dadaf2fb117
Elan Ruusamäe <glen@pld-linux.org>
2022-01-13 17:24:55 +0200

    Update test_api

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """trakt.tv functional tests"""
-from trakt.api import TraktApi
+from trakt.api import HttpClient
 from trakt.core import api, Alias
 from trakt.tv import TVShow
 
@@ -8,7 +8,7 @@ from trakt.tv import TVShow
 def test_api():
     api1 = api()
     api2 = api()
-    assert isinstance(api1, TraktApi)
+    assert isinstance(api1, HttpClient)
     assert api1 == api2
 
     show = TVShow('Game of Thrones')

7b4070a9ca14559174155adaee915916d49274f1
Elan Ruusamäe <glen@pld-linux.org>
2022-01-13 15:03:59 +0200

    Cleanup test

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 """trakt.tv functional tests"""
 from trakt.api import TraktApi
-from trakt.core import get_api
+from trakt.core import api
 from trakt.tv import TVShow
 
 
 def test_api():
-    api = get_api()
-    assert isinstance(api, TraktApi)
+    api1 = api()
+    api2 = api()
+    assert isinstance(api1, TraktApi)
+    assert api1 == api2
 
     show = TVShow('Game of Thrones')
     assert show.title == 'Game of Thrones'
-
-    print(show.new_get())

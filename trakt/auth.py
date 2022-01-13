0f7a9490f6ea76e41a05c779b77f6eabdfb5c51b
Elan Ruusamäe <glen@pld-linux.org>
2022-01-13 18:15:16 +0200

    Add auth init()

--- trakt/auth.py
+++ trakt/auth.py
@@ -2,3 +2,26 @@
 """Authentication methods"""
 
 __author__ = 'Jon Nappi, Elan Ruusamäe'
+
+from trakt import PIN_AUTH, OAUTH_AUTH, DEVICE_AUTH
+
+
+def pin_auth():
+    pass
+
+
+def oauth_auth():
+    pass
+
+
+def device_auth():
+    pass
+
+
+def init(AUTH_METHOD, *args, **kwargs):
+    auth_method = {
+        PIN_AUTH: pin_auth, OAUTH_AUTH: oauth_auth, DEVICE_AUTH: device_auth
+    }
+
+    """Run the auth function specified by *AUTH_METHOD*"""
+    return auth_method.get(AUTH_METHOD, PIN_AUTH)(*args, **kwargs)

b5d93cdbd78a115027ce779945526ab289bf1db0
Elan Ruusamäe <glen@pld-linux.org>
2022-01-14 20:18:11 +0200

    fixup! Guard TokenAuth to skip oauth requests

--- trakt/api.py
+++ trakt/api.py
@@ -107,10 +107,11 @@ class TokenAuth(AuthBase):
         self.config = config
         self.client = client
         self.logger = logging.getLogger('trakt.api.token_auth')
+        self.base_url = f'{self.client.base_url.rstrip("/")}/oauth/'
 
     def __call__(self, r):
         # Skip oauth requests
-        if r.url.startswith(f'{self.client.base_url}/oauth/'):
+        if r.url.startswith(self.base_url):
             return r
 
         [client_id, client_token] = self.get_token()

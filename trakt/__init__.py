4a2d6f4431ca683561d8f74c14c24fcdc4c2b0a0
Elan Ruusamäe <glen@pld-linux.org>
2022-10-23 17:46:09 +0300

    Set version to 3.4.4

--- trakt/__init__.py
+++ trakt/__init__.py
@@ -5,6 +5,6 @@ try:
 except ImportError:
     pass
 
-version_info = (3, 4, 3)
+version_info = (3, 4, 4)
 __author__ = 'Jon Nappi, Elan Ruusamäe'
 __version__ = '.'.join([str(i) for i in version_info])
